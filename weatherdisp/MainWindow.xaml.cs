aab88df40dabc02c49d54b78d23aaccb43c42ae2
weloytty <bill@loytty.com>
2013-11-10 11:23:32 -0500

    implmented dispose so code analysis was happy wtih me and added indoor temp/humidity

--- weatherdisp/MainWindow.xaml.cs
+++ weatherdisp/MainWindow.xaml.cs
@@ -21,40 +21,27 @@ namespace weatherdisp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window , IDisposable
+    public partial class MainWindow : Window, IDisposable
     {
         FileSystemWatcher _watch;
         public MainWindow()
         {
             _watch = new FileSystemWatcher();
             InitializeComponent();
-            
         }
 
         ~MainWindow()
         {
-            _watch.Dispose();
+            Dispose(false);
         }
 
-        
+
 
 
         private void Grid_Initializer(object sender, EventArgs e)
         {
-            WeatherData.WeatherReading r = new WeatherReading();
-            this.outdoorTemperature.Value = r.OutdoorTemperature;
-            this.temperatureLabel.Content = r.OutdoorTemperature;
 
-            String risingOrFalling = "";
-            if (r.WeatherTendancy == WeatherReading.WeatherTendancies.Rising) risingOrFalling = "↑";
-            if (r.WeatherTendancy == WeatherReading.WeatherTendancies.Falling) risingOrFalling = "↓";
-
-            this.pressureLabel.Content = r.PressureRelative + risingOrFalling;
-            this.atmosphericPressure.Value = r.PressureRelative ;
-            this.outdoorHumidityLabel.Content = r.OutdoorHumidity.ToString()  +"%";
-            this.outdoorHumidity.Value = r.OutdoorHumidity ;
-            this.dataUpdated.Content = r.DataRead;
-
+            updateDisplay(new WeatherReading());
 
             if (File.Exists(Environment.GetEnvironmentVariable("ALLUSERSPROFILE") + @"\currdat.lst"))
             {
@@ -64,6 +51,7 @@ namespace weatherdisp
 
                 _watch.Changed += new FileSystemEventHandler(watch_Changed);
                 _watch.EnableRaisingEvents = true;
+
             }
 
         }
@@ -71,35 +59,49 @@ namespace weatherdisp
         void watch_Changed(object sender, FileSystemEventArgs e)
         {
 
-            WeatherReading newReading = new WeatherReading();
 
-            this.Dispatcher.Invoke((Action)(()=>
+            this.Dispatcher.Invoke((Action)(() =>
             {
-                this.outdoorTemperature.Value = newReading.OutdoorTemperature;
-                this.temperatureLabel.Content = newReading.OutdoorTemperature;
-
-                String risingOrFalling = "";
-                if (newReading.WeatherTendancy == WeatherReading.WeatherTendancies.Rising) risingOrFalling = "↑";
-                if (newReading.WeatherTendancy == WeatherReading.WeatherTendancies.Falling) risingOrFalling = "↓";
-
-                this.pressureLabel.Content = newReading.PressureRelative + risingOrFalling;
-                this.atmosphericPressure.Value = newReading.PressureRelative;
-                this.dataUpdated.Content = newReading.DataRead;
-                this.outdoorHumidity.Value = newReading.OutdoorHumidity;
-                this.outdoorHumidityLabel.Content = newReading.OutdoorHumidity.ToString() + "%"; 
-
+                updateDisplay(new WeatherReading());
             }));
-
-
-            
-
 
         }
 
+        void updateDisplay(WeatherReading r)
+        {
+            this.outdoorTemperature.Value = r.OutdoorTemperature;
+            this.temperatureLabel.Content = r.OutdoorTemperature;
+            this.indoorTemperatureLabel.Content = r.IndoorTemperature;
+            this.indoorTemperature.Value = r.IndoorTemperature;
+
+            String risingOrFalling = "";
+            if (r.WeatherTendancy == WeatherReading.WeatherTendancies.Rising) risingOrFalling = "↑";
+            if (r.WeatherTendancy == WeatherReading.WeatherTendancies.Falling) risingOrFalling = "↓";
+
+            this.pressureLabel.Content = r.PressureRelative + risingOrFalling;
+            this.atmosphericPressure.Value = r.PressureRelative;
+            this.outdoorHumidityLabel.Content = r.OutdoorHumidity.ToString() + "%";
+            this.outdoorHumidity.Value = r.OutdoorHumidity;
+            this.indoorHumidity.Value = r.IndoorHumidity;
+            this.indoorHumidityLabel.Content = r.IndoorHumidity.ToString() + "%";
+
+            this.dataUpdated.Content = r.DataRead;
+
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing && _watch != null)
+            {
+                _watch.Dispose();
+                _watch = null;
+            }
+        }
 
         public void Dispose()
         {
-            _watch.Dispose();
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
